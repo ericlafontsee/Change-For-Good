bfec14b9d24ecd8f6b46ae57b540b3ece98f26a6
Haley Myers <haleynmyers@gmail.com>
2020-10-01 14:38:09 -0500

    Added random org id to each event

--- testEvent.js
+++ testEvent.js
@@ -4,18 +4,23 @@ const fakerator = Fakerator();
 const { v4: uuidv4 } = require("uuid");
 const testEvents = [];
 
-for (let i = 0; i < 10; i++) {
-  const obj = {};
-  obj.id = uuidv4();
-  obj.title = fakerator.lorem.word();
-  obj.description = fakerator.lorem.sentence();
-  obj.eventDate = fakerator.date.future(1, "2020-10-12");
-  testEvents.push(obj);
-}
-
-console.log(testEvents);
-
 db.sequelize.sync().then(() => {
-  console.log(testEvents);
-  db.Event.bulkCreate(testEvents);
+  db.Organization.findAll({}).then(results => {
+    
+    for (let i = 0; i < 10; i++) {
+      const random = Math.floor(Math.random() * results.length);
+      const randomOrg = JSON.parse(JSON.stringify(results[random]));
+      const obj = {};
+      obj.id = uuidv4();
+      obj.title = fakerator.lorem.word();
+      obj.description = fakerator.lorem.sentence();
+      obj.eventDate = fakerator.date.future(1, "2020-10-12");
+      obj.OrganizationId = randomOrg.id;
+      testEvents.push(obj);
+    }
+
+    console.log(testEvents);
+
+    db.Event.bulkCreate(testEvents);
+  });
 });
