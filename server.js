6eb144f03ecc8bda7113dcf5ca19731c4957b53c
Eric LaFontsee <elafontsee@gmail.com>
2020-10-01 14:46:10 -0400

    Tested the get all route for organizations

--- server.js
+++ server.js
@@ -22,13 +22,13 @@ app.use(passport.session());
 
 // Requiring our routes
 require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+require("./routes/event-api-routes.js")(app);
 require("./routes/org-api-routes.js")(app);
 require("./routes/ind-api-routes.js")(app);
 
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
