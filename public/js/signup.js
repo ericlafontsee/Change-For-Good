79418a4f6da830ac802e1c34ff3385daa8123e08
Miller Rich <william.miller.rich@gmail.com>
2020-10-01 18:34:14 -0500

    add jquery cdn to signup.html, update functions in signup.js to include name, insert console.log to test form

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,34 +1,38 @@
 $(document).ready(() => {
   // Getting references to our form and input
   const signUpForm = $("form.signup");
+  // const button = $("button.loginBtn");
   const emailInput = $("input#email");
   const passwordInput = $("input#password");
-  const passwordConfirm = $("input#passwordConfirm");
-  const telephone = $("input#orgPhone");
-  const website = $("input#orgSite");
+  const name = $("input#name");
+  // const passwordConfirm = $("input#passwordConfirm");
+  // const telephone = $("input#orgPhone");
+  // const website = $("input#orgSite");
 
   // When the signup button is clicked, we validate the email and password are not blank
   signUpForm.on("submit", event => {
     event.preventDefault();
     const userData = {
       email: emailInput.val().trim(),
+      name: name.val().trim(),
       password: passwordInput.val().trim()
     };
-
+    console.log(userData);
     if (!userData.email || !userData.password) {
       return;
     }
     // If we have an email and password, run the signUpUser function
-    signUpUser(userData.email, userData.password);
+    signUpUser(userData.email, userData.name, userData.password);
     emailInput.val("");
     passwordInput.val("");
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(email, password) {
+  function signUpUser(email, name, password) {
     $.post("/api/signup", {
       email: email,
+      name: name,
       password: password
     })
       .then(() => {
@@ -44,11 +48,11 @@ $(document).ready(() => {
   }
 });
 
-function passwordCheck(password, confirm) {
-  if (password !== confirm) {
-    alert("Your password does not match, please enter again");
-    passwordInput.empty();
-    passwordConfirm.empty();
-    return;
-  }
-}
+// function passwordCheck(password, confirm) {
+//   if (password !== confirm) {
+//     alert("Your password does not match, please enter again");
+//     passwordInput.empty();
+//     passwordConfirm.empty();
+//     return;
+//   }
+// }
