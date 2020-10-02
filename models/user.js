a4a6fa2df15095fd2c7ddd671aabafff4bb5ae4b
Miller Rich <william.miller.rich@gmail.com>
2020-10-02 09:35:32 -0500

    update bcrypt functions from arrow functions to fix issues with scope

--- models/user.js
+++ models/user.js
@@ -34,14 +34,14 @@ module.exports = (sequelize, DataTypes) => {
     });
   };
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-  User.prototype.validPassword = password => {
+  User.prototype.validPassword = function(password)  {
     console.log(password);
     console.log(this.password);
     return bcrypt.compareSync(password, this.password);
   };
   // Hooks are automatic methods that run during various phases of the User Model lifecycle
   // In this case, before a User is created, we will automatically hash their password
-  User.addHook("beforeCreate", (user) => {
+  User.addHook("beforeCreate", function(user) {
     user.password = bcrypt.hashSync(
       user.password,
       bcrypt.genSaltSync(10),
