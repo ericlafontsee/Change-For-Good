6cabb10e99291b8ebe8ed3110b328b2ffd2e4e44
Payton Banks <payton.banks@gmail.com>
2020-10-03 14:50:38 -0400

    Added public/images. Base styling of user pages. Icon added to header.

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const express = require("express");
 const session = require("express-session");
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
+var exphbs = require("express-handlebars");
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
