b2d3ea5efce73d6230519e9576ba41f4099c020b
Eric LaFontsee <elafontsee@gmail.com>
2020-10-04 17:03:07 -0500

    Added the grid system to the userlanding page so the browse events and saved events were side by side

--- config/passport.js
+++ config/passport.js
@@ -50,21 +50,21 @@ passport.use(
         where: {
           email: email
         }
-      }).then(dbUser => {
+      }).then(dbOrg => {
         // If there's no user with the given email
-        if (!dbUser) {
+        if (!dbOrg) {
           return done(null, false, {
             message: "Incorrect email."
           });
         }
         // If there is a user with the given email, but the password the user gives us is incorrect
-        else if (!dbUser.validPassword(password)) {
+        else if (!dbOrg.validPassword(password)) {
           return done(null, false, {
             message: "Incorrect password."
           });
         }
         // If none of the above, return the user
-        return done(null, dbUser);
+        return done(null, dbOrg);
       });
     }
   )
